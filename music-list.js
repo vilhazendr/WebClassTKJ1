3a22ab0ede4770b243c00fec3304c79d7d7c63bf
vilhazendr <vilhazendr@gmail.com>
2026-08-25 00:16:06 +0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,9 @@
+// Musik yang tersedia di Music Player.
+// File MP3 berada di folder assets/music/
+window.LOCAL_MUSIC = [
+  {
+    name: "Please Don't Call - Bleachers",
+    src: "assets/music/Eid-Adha%2C%20Please%20Don't%20Call%20-%20Bleachers.mp3",
+    type: "audio/mpeg"
+  }
+];
